25d4f7d94d8e68b565926ad402eff3924d23a55b
Federico Lo Grasso <lograssofede@gmail.com>
2017-06-12 17:45:43 -0300

    Delete debug code

--- Server_DoorOpen_C#/Server_DoorOpen/TCPSocketListener.cs
+++ Server_DoorOpen_C#/Server_DoorOpen/TCPSocketListener.cs
@@ -99,7 +99,7 @@ namespace Server_DoorOpen
 					
 					/*Convierto el paquete recibido a un objeto mensaje*/
 					string aux = System.Text.Encoding.UTF8.GetString(byteBuffer);
-					MessageBox.Show(aux);
+					//MessageBox.Show(aux);
 					if (aux.IndexOf("Enviar") != -1)
 					{
 						TCPServer.queue.Enqueue("Enviar");
