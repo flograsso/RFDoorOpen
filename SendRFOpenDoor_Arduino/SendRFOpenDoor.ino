50fb787d4e51f45abe2e61a8a7c6a62d2549ad9e
Federico <lograssofede@gmail.com>
2018-07-07 09:46:48 -0300

    Agregado de funcionalidad de botón físico

--- SendRFOpenDoor_Arduino/SendRFOpenDoor.ino
+++ SendRFOpenDoor_Arduino/SendRFOpenDoor.ino
@@ -3,9 +3,11 @@ Codigo que envia el comando de apertura de la puerta por RF si recibe el string 
 */
 
 #include <RCSwitch.h>
-
+const int buttonPin = 2; 
 RCSwitch mySwitch = RCSwitch();
 String incomingByte;   // for incoming serial data
+mySwitch.send(8059025,24);
+
 void setup() 
 {
 
@@ -17,6 +19,7 @@ void setup()
 	//LED Integrado a la placa
 	pinMode(LED_BUILTIN, OUTPUT);
 	digitalWrite(LED_BUILTIN, LOW); 
+ 	pinMode(buttonPin, INPUT); //Boton
 	  
 
 }
@@ -38,7 +41,12 @@ void loop()
 			digitalWrite(LED_BUILTIN, HIGH); 
 		}
     }
+    buttonState = digitalRead(buttonPin);
+      if (buttonState == HIGH) {
+    // turn LED on:
+      mySwitch.send(8059025,24);
+  } 
 }
 
 
-
+	
